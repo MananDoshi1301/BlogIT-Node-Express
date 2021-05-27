d8c2eab61c59a096698af8569f01b5a493b0dc45
Manan Doshi <manandoshi1301@gmail.com>
2021-05-28 00:06:36 +0530

    Project complete with mvc structure

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const blogRoutes = require('./routes/blogRoutes');
 
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 //dBConnection
 const dbUri = "mongodb+srv://manan123:manan123@blogitcluster.pa4zc.mongodb.net/Node-Tut?retryWrites=true&w=majority";
@@ -23,47 +23,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {  
-  Blog.find().sort({createdAt:-1})
-  .then(result=>{
-    blogs = result;
-    res.render('index', { blogs });
-  })
-  .catch(err=>console.log(err.message));
+  res.redirect('/blogs')
 })
 
-app.get('/blogs', (req, res) => {
-  res.redirect('/');
-})
-
-app.post('/blogs',(req, res)=>{
-  const blog = new Blog(req.body);
-  blog.save().then(result=>{
-    res.redirect('/')
-  }).catch(err=>console.log(err.message));
-})
-
-app.get('/about', (req, res) => {
-  res.render('about')
-})
-
-app.get('/blog/create', (req, res) => {
-  res.render('create');
-})
-
-app.get('/blogs/:id', (req,res)=>{
-  const id = req.params.id;
-  Blog.findById(id)
-  .then(result=>{
-    res.render('blog', {blog:result})
-  }).catch(err=>console.log(err.message));
-})
-
-app.delete('/blogs/:id', (req, res)=>{
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id).then((result)=>{
-    res.json({redirect:'/blogs'})
-  }).catch(err=>console.log(err.message));
-})
+app.use('/blogs',blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render('404');
