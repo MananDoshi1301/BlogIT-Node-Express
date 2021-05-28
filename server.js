ffc5b77cb8e1a18183476e0df2e7d588dc239098
MananDoshi1301 <manandoshi1301@gmail.com>
2021-05-28 12:01:38 +0530

    AOS script for scroll added

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 3000;
 
 //dBConnection
 const dbUri = "mongodb+srv://manan123:manan123@blogitcluster.pa4zc.mongodb.net/Node-Tut?retryWrites=true&w=majority";
-mongoose.connect(dbUri, {useUnifiedTopology:true, useNewUrlParser:true})
-.then(result=>app.listen(port))
-.catch(err=>console.log(err.message));
+mongoose.connect(dbUri, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(result => app.listen(port))
+  .catch(err => console.log(err.message));
 
 // register view engine
 app.set("view engine", 'ejs');
@@ -19,14 +19,14 @@ app.set("view engine", 'ejs');
 console.log('Server Running', 'on port', port);
 
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {  
+app.get('/', (req, res) => {
   res.redirect('/blogs')
 })
 
-app.use('/blogs',blogRoutes);
+app.use('/blogs', blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render('404');
